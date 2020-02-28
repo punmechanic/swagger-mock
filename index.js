023329b3e6fc9595cd8e7143128232f898e48980
Dan Pantry <devnull@danpantry.me>
2020-02-28 20:39:38 +0000

    rejig things around a lot

--- index.js
+++ index.js
@@ -1,141 +1,29 @@
-const http = require("http");
-const url = require("url");
-const express = require("express");
-const yaml = require("js-yaml");
-const { resolveSpecification } = require("./specification");
-const generateResponse = require("./responses");
-const { emptyGenerator, request, cyclicGenerator } = require("./util");
 const yargs = require("yargs");
 const fs = require("fs").promises;
 const path = require("path");
+const { createServer, cyclicGenerator } = require("./lib");
 
-class MockSwaggerServer {
-  constructor(handler) {
-    this._handler = handler;
-  }
+yargs
+  .number("port")
+  .string("host")
+  .string("specification")
+  .describe(
+    "specification",
+    "The path to the YAML specification file for your Swagger API"
+  )
+  .demandOption(["port", "host", "specification"]);
 
-  start(port = 0, bindAddress = "127.0.0.1") {
-    const handle = http.createServer(this._handler);
-    return new Promise((resolve, reject) => {
-      handle.once("error", reject);
-      handle.once("listening", () => {
-        this._handle = handle;
-        resolve();
-      });
+const argv = yargs.argv;
 
-      handle.listen(port, bindAddress);
-    });
-  }
-
-  request(path) {
-    if (this._handle === undefined) {
-      return Promise.reject();
-    }
-
-    const addr = this._handle.address();
-    const uri = url.format({
-      protocol: "http",
-      hostname: addr.address,
-      port: addr.port,
-      pathname: path
-    });
-
-    return request(uri);
-  }
-
-  stop() {
-    if (this._handle === undefined) {
-      return;
-    }
-
-    this._handle.close();
-  }
-}
-
-function generateMiddleware(pattern, spec, generator) {
-  // Generating an express app for all of these is probably quite wasteful, but it certainly works for an MVP
-  const route = express();
-
-  // We will also need to configure methods. The swagger spec gives the ability for you to use other verbs.
-  route.get(pattern, (req, res) => {
-    // By default, let's just assume we are always going to return a HTTP 200 response
-    if ("200" in spec.responses === false) {
-      // TODO: Utilize the next() parameter instead of this
-      const payload = {
-        message: `could not return a response for ${req.url} because there was no HTTP 200 response given in the specification`
-      };
-      const jsonified = JSON.stringify(payload);
-      res.statusCode = 500;
-      res.write(jsonified);
-      res.end();
-      return;
-    }
-
-    const responseSpec = spec.responses["200"];
-    const data = generateResponse(responseSpec, generator);
-    const jsonified = JSON.stringify(data);
-    res.write(jsonified);
-    res.end();
-  });
-
-  return route;
-}
-
-function generateHandler(spec, valueGenerator) {
-  // Only one generator for now, which is obviously incorrect, but it should probably be app-wide.
-  const generator = valueGenerator();
-  const app = express();
-
-  const patterns = Object.keys(spec.paths);
-  const middlewares = patterns.map(pattern =>
-    generateMiddleware(pattern, spec.paths[pattern], generator)
-  );
-
-  app.use(...middlewares);
-
-  app.use((_req, res, _next) => {
-    res.writeHead(404);
-    res.end();
-  });
-
-  return app;
-}
-
-function createServer(yamlDocString, valueGenerator = emptyGenerator) {
-  const parsed = yaml.safeLoad(yamlDocString);
-  const specification = resolveSpecification(parsed);
-  const handler = generateHandler(specification, valueGenerator);
-  return new MockSwaggerServer(handler);
-}
-
-// I'm not a fan of CommonJS exports but tape cli doesn't let us use ES6 ones.
-module.exports = createServer;
-module.exports.resolveSpecification = resolveSpecification;
-
-async function main() {
-  yargs
-    .number("port")
-    .string("host")
-    .string("specification")
-    .describe(
-      "specification",
-      "The path to the YAML specification file for your Swagger API"
-    )
-    .demandOption(["port", "host", "specification"]);
-
-  const argv = yargs.argv;
-  const spec = await fs.readFile(
-    path.resolve(process.cwd(), argv.specification)
-  );
+async function main(port, host, specification) {
+  const spec = await fs.readFile(path.resolve(process.cwd(), specification));
 
   const placeholderGenerator = cyclicGenerator(["foo", "bar", "baz"]);
   const server = createServer(
     spec.toString("ascii"),
     () => placeholderGenerator
   );
-  server.start(argv.port, argv.host);
+  server.start(port, host);
 }
 
-if (require.main.filename === __filename) {
-  main();
-}
+main(argv.port, argv.host, argv.specification);
